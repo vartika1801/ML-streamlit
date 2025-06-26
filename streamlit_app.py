17d3762424040c301ef40320182b32573b564ffb
Vartika <149591993+vartika1801@users.noreply.github.com>
2025-06-26 23:40:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('🎈 App Name')
+st.title('Machine Learning App')
 
-st.write('Hello world!')
+st.info('This is an app for Machine learning model')
+
+with expander('Data'):
+  st.write('**Raw Data**')
+  df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+  st.write('**X**')
+  X=df.drop('species',axis=1)
+  X
+  st.write('**y**')
+  y=df['species']
+  y
+
