fad82896e830449b5a993f94de664e554bad1fd0
Vartika <149591993+vartika1801@users.noreply.github.com>
2025-06-26 23:40:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('Machine Learning App')
 
 st.info('This is an app for Machine learning model')
 
-with expander('Data'):
+with st.expander('Data'):
   st.write('**Raw Data**')
   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
