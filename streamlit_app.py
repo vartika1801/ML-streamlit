2d39f9e9e1d633227ce96fa54e7d49afa4c82992
Vartika <149591993+vartika1801@users.noreply.github.com>
2025-06-27 00:02:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,37 @@ with st.expander('Data'):
   y=df['species']
   y
 
+with st.expander('Data Visualization : '):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+# Data Preparations
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  gender = st.selectbox('Gender', ('male','female'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+
+# Create a dataframe for input features 
+data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+input_df=pd.DataFrame(data,index=[0])
+input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+with st.expander('Input features'):
+  st.write('**Input Penguin**')
+  input_df
+  st.write('**Combined Data**')
+  input_penguins
+
+
+# Encode
+encode=['island','sex']
+  
+
